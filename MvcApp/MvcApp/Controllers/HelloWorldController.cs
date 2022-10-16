8c5c3253ae16069d85402272a5ab48c45033dea2
Leonardo Castilho <79671553+CyborgLeunardu@users.noreply.github.com>
2022-10-16 19:44:20 -0300

    AppDog
    Navegação de Páginas
    Criação de Form
    Data Binding

---
+++ MvcApp/MvcApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp.Models;
+using System.Reflection.Metadata;
+
+namespace MvcApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            
+            return View(dogs);
+
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public String Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
